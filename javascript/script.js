2fc4b65a1d2fe9957296a255a3977f3987e20314
drewheathens <mudavadie@gmail.com>
2018-12-05 17:50:07 +0300

    scalene working, readme added BDD

--- javascript/script.js
+++ javascript/script.js
@@ -19,6 +19,7 @@ function start(){
   var add = function(number1, number2, number3) {
  return number1 + number2 + number3;
 };
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var number3 = parseInt(prompt("Enter the last number:"));

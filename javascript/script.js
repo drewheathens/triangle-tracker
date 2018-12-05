708bb426d8d3152aabf2224e00de65df70596568
drewheathens <mudavadie@gmail.com>
2018-12-05 18:05:16 +0300

    scalene working, readme added BDD

--- javascript/script.js
+++ javascript/script.js
@@ -16,21 +16,19 @@
 // }
 
 function start(){
-  var add = function(number1, number2, number3) {
- return number1 + number2 + number3;
-};
+
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var number3 = parseInt(prompt("Enter the last number:"));
-var result = add(number1, number2, number3);
-alert(result);
+
 
 if (number1===number2 && number2===number3) alert("equilateral!")
 
 else if(number1===number2 || number2===number3) alert("isosceles!");
 
-else if (number1+number2<=number3 || number2+number3<=number1) alert("FATAL ERROR! triangle cannot be formed using those values.");
+else if (number1+number2<=number3 || number2+number3<=number1) alert(" triangle cannot be formed using those values!");
 
+else if (number1!==number2 && number2!==number3 &&  number3!==number1) alert("scalene!")
 
-}
+};

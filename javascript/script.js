880e668ad47abb41431afba04ab21d4ee41d219f
drewheathens <mudavadie@gmail.com>
2018-12-06 09:02:01 +0300

    js must enter number

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,3 @@
-// var add = function(number1, number2, number3) {
-//   return number1 + number2 + number3;
-// };
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var number3 = parseInt(prompt("Enter the last number:"));
-// var result = add(number1, number2, number3);
-// alert(result);
-//
-//  if (number1===number2 && number2===number3) {console.log("equilateral!");
-//
-// } else if(number1===number2 || number2===number3) {console.log("isosceles!");
-//
-// } else if (number1+number2>=number3 || number2+number3<=number1) {console.log("FATAL ERROR! cannot be formed using those values.");
-//
-// }
 
 function start() {
 

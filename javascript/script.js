dd743d72067a5990354a0a36e0e4d07124bf65b5
drewheathens <mudavadie@gmail.com>
2018-12-06 08:50:02 +0300

    reviewed js file

--- javascript/script.js
+++ javascript/script.js
@@ -15,20 +15,22 @@
 //
 // }
 
-function start(){
+function start() {
+
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var number3 = parseInt(prompt("Enter the last number:"));
 
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var number3 = parseInt(prompt("Enter the last number:"));
-
-
-if (number1===number2 && number2===number3) alert("equilateral!")
-
-else if(number1===number2 || number2===number3) alert("isosceles!");
-
-else if (number1+number2<=number3 || number2+number3<=number1) alert(" triangle cannot be formed using those values!");
-
-else if (number1!==number2 && number2!==number3 &&  number3!==number1) alert("scalene!")
-
-};
+  if (number1 + number2 <= number3 || number1 + number3 <= number2 || number2 + number3 <= number1) {
+    alert("Not a Triangle!");
+  } else if (number1 === number2 && number2 === number3) {
+    alert("equilateral!")
+  } else if (number1 === number2 || number2 === number3) {
+    alert("isosceles!");
+  } else if (number1 + number2 >= number3 || number1 + number3 >= number2 || number2 + number3 >= number1) {
+    alert("scalene!");
+  } else {
+    alert("You must enter number value");
+  }
+}
